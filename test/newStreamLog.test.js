85e7a9994bac6fcc9439e4e138c4a616a0f13fc2
Jack Ralph <jackralph@Jacks-MBP.local>
2020-08-05 22:17:14 +0100

    missing authentication token error

--- test/newStreamLog.test.js
+++ test/newStreamLog.test.js
@@ -1,26 +1,9 @@
-const {DocumentClient} = require('aws-sdk/clients/dynamodb');
+const axios = require("axios");
 
-const isTest = process.env.JEST_WORKER_ID;
-const config = {
-  convertEmptyValues: true,
-  ...(isTest && {
-    endpoint: 'localhost:8000',
-    sslEnabled: false,
-    region: 'local-env',
-  }),
-};
-
-const ddb = new DocumentClient(config);
-
-it('should insert item into table', async () => {
-    await ddb
-      .put({TableName: 'files', Item: {id: '1', hello: 'world'}})
-      .promise();
-  
-    const {Item} = await ddb.get({TableName: 'files', Key: {id: '1'}}).promise();
-  
-    expect(Item).toEqual({
-      id: '1',
-      hello: 'world',
+describe("logStream()", () => {
+  it("returns 201", () => {
+    axios.post("https://vra4plkr9e.execute-api.eu-west-2.amazonaws.com/dev", {
+      userId: "12345",
     });
   });
+});
